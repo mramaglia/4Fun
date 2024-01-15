8362c2f6019aabf4eb72727cb7f5f5bd756e018c
labro <labroccamarco@gmail.com>
2024-01-15 22:55:34 +0100

    Aggiunta ImagePicker più altre cose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,13 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //imagepicker
+    implementation ("com.github.dhaval2404:imagepicker:2.1")
 
     //slider images
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
